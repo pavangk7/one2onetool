8c2b9919166732205fa71e1e3d14286aa8960f65
Pavan Kumar <pavan.k.guniganti@gmail.com>
2022-09-15 02:46:26 +0800

    Changing the config

--- startup.sh
+++ startup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-#testing
-exec npm start $DATA_FILE
+set -x
+IP=$(ip route show |grep -o src.* |cut -f2 -d" ")
+#BRANCH_NAME="staging"
+# kubernetes sets routes differently -- so we will discover our IP differently
+if [[ ${BRANCH_NAME} == "staging" ]]; then
+  exec npm start DATA_FILE="Questions-test.json"
+else
+  exec npm start
+fi
+SUBNET=$(echo ${IP} | cut -f1 -d.)
+NETWORK=$(echo ${IP} | cut -f3 -d.)
+

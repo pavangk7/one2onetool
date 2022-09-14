3e7a4cb42b5e657d543d9d51a779160137fe58c5
Pavan Kumar <pavan.k.guniganti@gmail.com>
2022-09-15 00:46:14 +0800

    Changing the config

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -24,31 +23,3 @@ app.set('port', port);
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });
-=======
-const config =  require('./config.js');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
-const app = express();
-
-// View enginer setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// Middleware setup
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Mount routes
-app.use('/', routes);
-
-// Start server
-var port = process.env.PORT || '3000';
-app.set('port', port);
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
->>>>>>> master

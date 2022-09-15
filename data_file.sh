83fcf204be7ae46fb6c02dda02a47dfe6d1e7441
Pavan Kumar <pavan.k.guniganti@gmail.com>
2022-09-15 19:50:43 +0800

    Changing the config

--- data_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter password"
-export BRANCH="staging"
-if [ $BRANCH="staging" ]
-then
-  export DATA_FILE="Questions-test.json"
-else
-  echo "The password is incorrect, try again."
-fi
